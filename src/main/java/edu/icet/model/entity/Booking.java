340a86cb5ed78740ed66dd7ecb67bb0670f65bca
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-21 03:36:51 +0530

    Add Booking entity class

--- src/main/java/edu/icet/model/entity/Booking.java
+++ src/main/java/edu/icet/model/entity/Booking.java
@@ -1,4 +1,21 @@
 package edu.icet.model.entity;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Booking {
+
+    private long id;            // PK, auto-increment
+    private long userId;        // FK → users.id
+    private long seatId;        // FK → seats.id
+    private BigDecimal amountPaid; // amount paid for the Booking
+    private String status;      // CONFIRMED / CANCELLED
+    private LocalDateTime bookedAt; // timestamp of booking
 }
